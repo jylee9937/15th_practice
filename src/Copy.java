98f8a6b933b843347218451ce467b273a06b8815
jylee9937 <48907146+jylee9937@users.noreply.github.com>
2021-06-14 17:34:54 +0900

    textFile 읽고, 쓰기 성공 textFile 복사하기 성공

--- src/Copy.java
+++ src/Copy.java
@@ -13,16 +13,21 @@ public class Copy {
     }
     public static void doCopy(String[] args)throws IOException{
         int i;
-        Path inputPath = Paths.get("/Users/a123123/IdeaProjects/15주차_실습과제/out/production/15주차_실습과제/"+ args[0]); //첫번째 인자로 받은 파일명의 경로 수집
-        Path outputPath = Paths.get("/Users/a123123/IdeaProjects/15주차_실습과제/out/production/15주차_실습과제/"+args[1]); // 두번째 인자로 받은 파일명의 경로 수집
+        Path inputPath = Paths.get("/Users/a123123/IdeaProjects/15주차_실습과제/"+args[0]); //첫번째 인자로 받은 파일명의 경로 수집
+        Path outputPath = Paths.get("/Users/a123123/IdeaProjects/15주차_실습과제/"+args[1]); // 두번째 인자로 받은 파일명의 경로 수집
 
-        try (InputStream in = Files.newInputStream(inputPath)) {
-            do{
-                i = in.read();
-                try (OutputStream out = Files.newOutputStream(outputPath)) {
-                    out.write((byte)i);
-                }
-            }while(i != -1);
+        InputStream in = Files.newInputStream(inputPath);
+        OutputStream out = Files.newOutputStream(outputPath);
+
+        while(true) {
+            int data = in.read();
+            if(data == -1) {
+                System.out.println("파일 끝");
+                break;
+            }
+            out.write(data);            // 한 바이트씩 쓴다.
         }
+        in.close();                        // 닫아주기
+        out.close();                        // 닫아주기
     }
 }
